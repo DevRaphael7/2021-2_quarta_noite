312e6c63c0b5daa6e31d88551da1caff93375200
traue <t.traue@gmail.com>
2021-09-22 20:49:32 -0300

    Projeto - Soma(a, n)

---
+++ Soma AN/Soma AN/FrmPrincipal.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Soma_AN
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btCalcular = new System.Windows.Forms.Button();
+            this.lbValorA = new System.Windows.Forms.Label();
+            this.lbValorN = new System.Windows.Forms.Label();
+            this.lbResultado = new System.Windows.Forms.Label();
+            this.txtValorA = new System.Windows.Forms.TextBox();
+            this.txtValorN = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btCalcular
+            // 
+            this.btCalcular.Location = new System.Drawing.Point(12, 97);
+            this.btCalcular.Name = "btCalcular";
+            this.btCalcular.Size = new System.Drawing.Size(145, 34);
+            this.btCalcular.TabIndex = 0;
+            this.btCalcular.Text = "Calcular Soma(a, N)";
+            this.btCalcular.UseVisualStyleBackColor = true;
+            this.btCalcular.Click += new System.EventHandler(this.btCalcular_Click);
+            // 
+            // lbValorA
+            // 
+            this.lbValorA.AutoSize = true;
+            this.lbValorA.Location = new System.Drawing.Point(12, 41);
+            this.lbValorA.Name = "lbValorA";
+            this.lbValorA.Size = new System.Drawing.Size(21, 17);
+            this.lbValorA.TabIndex = 1;
+            this.lbValorA.Text = "A:";
+            // 
+            // lbValorN
+            // 
+            this.lbValorN.AutoSize = true;
+            this.lbValorN.Location = new System.Drawing.Point(12, 72);
+            this.lbValorN.Name = "lbValorN";
+            this.lbValorN.Size = new System.Drawing.Size(20, 17);
+            this.lbValorN.TabIndex = 2;
+            this.lbValorN.Text = "n:";
+            // 
+            // lbResultado
+            // 
+            this.lbResultado.AutoSize = true;
+            this.lbResultado.Location = new System.Drawing.Point(12, 134);
+            this.lbResultado.Name = "lbResultado";
+            this.lbResultado.Size = new System.Drawing.Size(33, 17);
+            this.lbResultado.TabIndex = 3;
+            this.lbResultado.Text = "-----";
+            // 
+            // txtValorA
+            // 
+            this.txtValorA.Location = new System.Drawing.Point(39, 38);
+            this.txtValorA.Name = "txtValorA";
+            this.txtValorA.Size = new System.Drawing.Size(100, 22);
+            this.txtValorA.TabIndex = 4;
+            // 
+            // txtValorN
+            // 
+            this.txtValorN.Location = new System.Drawing.Point(38, 69);
+            this.txtValorN.Name = "txtValorN";
+            this.txtValorN.Size = new System.Drawing.Size(100, 22);
+            this.txtValorN.TabIndex = 5;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(255, 182);
+            this.Controls.Add(this.txtValorN);
+            this.Controls.Add(this.txtValorA);
+            this.Controls.Add(this.lbResultado);
+            this.Controls.Add(this.lbValorN);
+            this.Controls.Add(this.lbValorA);
+            this.Controls.Add(this.btCalcular);
+            this.MinimizeBox = false;
+            this.Name = "FrmPrincipal";
+            this.Text = "Soma(a,N)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btCalcular;
+        private System.Windows.Forms.Label lbValorA;
+        private System.Windows.Forms.Label lbValorN;
+        private System.Windows.Forms.Label lbResultado;
+        private System.Windows.Forms.TextBox txtValorA;
+        private System.Windows.Forms.TextBox txtValorN;
+    }
+}
+
---
+++ Soma AN/Soma AN/FrmPrincipal.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soma_AN
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int a, n;
+                //A conversão pode ser feita de qualquer uma das formas abaixo:
+                a = int.Parse(txtValorA.Text);
+                n = Convert.ToInt32(txtValorN.Text);
+                lbResultado.Text = SomaAN(a, n);
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show("Houve algum erro...\n\n" + ex.Message);
+            }
+        }
+
+        private String SomaAN(int a, int n)
+        {
+            String resultado = "";
+
+            int contador = a;
+            int aux = a;
+            resultado += a + " + ";
+            for (int i = 1; i < n; i++)
+            {
+                contador += (a + i); //é o mesmo que contador = contator + (a+1)
+                aux = a + i; //é o próximo valor a ser somado
+                resultado += aux;
+                if(i != (n - 1))
+                {
+                    resultado += " + ";
+                }
+            }
+
+            return (resultado + " = " + contador);
+        }
+    }
+}
